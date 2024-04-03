72f5ab7020e6ffd51e3c63947ab835f61a5ebfba
Sigit Wahyudi <44634025+sgitwhyd@users.noreply.github.com>
2024-04-03 23:45:40 +0700

    Feat/chart widget (#2)
    * style: sorting widget
    
    * feat: create transaction chart
    
    * refactor: remove unused import

---
+++ app/Filament/Widgets/TransactionChart.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Transaction;
+use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Carbon;
+
+class TransactionChart extends ChartWidget
+{
+    protected static ?string $heading;
+    protected int | string | array $columnSpan = '2';
+    protected static ?string $maxHeight = '500px';
+    
+
+    public function __construct()
+    {
+        Carbon::setLocale('id');
+        self::$heading = 'Jumlah Total Pembayaran Tiap Hari Pada Bulan ' . Carbon::now()->translatedFormat('F');
+    }
+
+    protected static ?int $sort = 1;
+
+    protected function getData(): array
+    {
+        Carbon::setLocale('id');
+        $currentMonth = Carbon::now();
+        $lastDay = $currentMonth->daysInMonth;
+
+        $arrayDateOfMonth = [];
+
+        for ($day = 1; $day <= $lastDay; $day++) {
+            $currentDate = Carbon::create($currentMonth->year, $currentMonth->month, $day);
+            $arrayDateOfMonth[] = $currentDate->translatedFormat('l-d-Y');
+
+        }
+
+        $startDate = Carbon::now()->startOfMonth();
+        $endDate = Carbon::now()->endOfMonth();
+
+        $datesForMonth = collect(Carbon::parse($startDate)->daysUntil($endDate)->toArray());
+        $totalTransactionInDate = $datesForMonth->map(
+            function ($date) {
+                return Transaction::where('status', 'paid')->whereDate('updated_at', $date->format('Y-m-d'))->count();
+            }
+        )->toArray();
+
+
+
+
+        return [
+             'datasets' => [
+                 [
+                     'label' => 'Jumlah Transaksi',
+                     'data' => $totalTransactionInDate,
+                 ],
+             ],
+             'labels' => $arrayDateOfMonth,
+         ];
+
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
